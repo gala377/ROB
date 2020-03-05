6739f7b24f35c8edba6586874ae226059eda130e
gala377 <r.galczak@gmail.com>
2020-03-05 17:48:14 +0100

    Removed commented out files

--- lab1/classifyiris.m
+++ lab1/classifyiris.m
@@ -2,7 +2,6 @@ function res = classifyiris(i, data, dist)
     sample = data(i, 1:4);
     data(i, :) = [];
     dinstances = dist(sample, data(:, 1:4));
-    % dinstances
     [~, row] = min(dinstances);
     res = data(row, end);
 endfunction
